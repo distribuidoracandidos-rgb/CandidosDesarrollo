570689125e1348776be7b5bc605c3631dad61520
Ivo Candido <distribuidoracandidos@gmail.com>
2026-08-06 13:40:37 -0300

    agregue boton de rapi

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,27 +1,49 @@
-import { FaBusSimple, FaWhatsapp } from "react-icons/fa6";
+import { FaBusSimple, FaShop, FaWhatsapp } from "react-icons/fa6";
 import "../styles/ServiceCard.css";
 
 function ServiceCard() {
   return (
-    <div className="servicio-card">
-      <div className="servicio-icono">
-        <FaBusSimple />
+    <>
+      <div className="servicio-card">
+        <div className="servicio-icono">
+          <FaBusSimple />
+        </div>
+
+        <div className="servicio-info">
+          <strong>Carga virtual y SUBE</strong>
+          <span>Servicios</span>
+        </div>
+
+        <a
+          className="servicio-boton"
+          href="https://wa.me/5493434162242?text=Hola..."
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaWhatsapp /> ¡Contáctanos!
+        </a>
       </div>
 
-      <div className="servicio-info">
-        <strong>Carga Virtual SUBE</strong>
-        <span>Servicios</span>
-      </div>
+      <div className="servicio-card">
+        <div className="servicio-icono">
+          <FaShop />
+        </div>
 
-      <a
-        className="servicio-boton"
-        href="https://wa.me/5493434162242?text=Hola%20Candido's%2C%20quiero%20consultar%20por%20la%20Carga%20Virtual%20de%20SUBE."
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <FaWhatsapp /> ¡Contáctanos!
-      </a>
-    </div>
+        <div className="servicio-info">
+          <strong>Rapipago</strong>
+          <span>Cobranza de facturas</span>
+        </div>
+
+        <a
+          className="servicio-boton"
+          href="https://wa.me/5493434162242?text=Hola%20Candido's,%20quiero%20consultar%20por%20Rapipago."
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaWhatsapp /> ¡Contáctanos!
+        </a>
+      </div>
+    </>
   );
 }
 
